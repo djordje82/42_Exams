0e8291a453ea5cca69ce83944fb8a0f8239ba8e2
me11st <144932340+me11st@users.noreply.github.com>
2024-11-06 15:03:42 +0200

    Update ft_strcmp.c

--- Rank_03_Level_1/ft_strcmp/ft_strcmp.c
+++ Rank_03_Level_1/ft_strcmp/ft_strcmp.c
@@ -9,7 +9,7 @@ int ft_strcmp(char *s1, char *s2)
     }
     return (*s1 - *s2);
 }
-
+/*
 int main(void)
 {
     char *s1 = "error";
@@ -18,3 +18,4 @@ int main(void)
     printf("%d\n", ft_strcmp(s1, s2));
     return (0);
 }
+*/
